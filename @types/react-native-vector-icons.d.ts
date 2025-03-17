7e705940db3de1804660c38b10417dfbe56223b0
Pablo Henrique <pabllobeg@gmail.com>
2025-03-17 16:09:02 -0300

    add vector itens types

---
+++ @types/react-native-vector-icons.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-native-vector-icons/EvilIcons';
+declare module 'react-native-vector-icons/SimpleLineIcons';
+declare module 'react-native-vector-icons/AntDesign';
+declare module 'react-native-vector-icons/Feather';
+declare module 'react-native-vector-icons/MaterialIcons';
